3648ec6ecb7c1baa7f988d52de7f127c34bda56e
joshua+github@developedsimple.com <joshua@developedsimple.com>
2013-01-25 21:38:38 -0500

    Add the annotated documentation for Geolocation, Initialize, and Basemaps;

--- js/init.js
+++ js/init.js
@@ -1,69 +1,53 @@
-/**
- * Developed Simple ArcGIS Web Application (c) 2013
- *
- *
- * This application was built by the folks at Viable for
- * use by it's clients. Any code herein is property of Developed
- * Simple unless where otherwise noted property of ESRI.
- *
- * To find out how you can use this software or have your
- * own version of this software built to fit your custom
- * needs, contact us by visiting www.developedsimple.com.
- *
- */
-var defaults = {}, requestHandle;
+// Setup the ``defaults`` object which sets options that will be
+// use throughout our application.
+var defaults = {};
 
+// Here we are going to load our primary map. We need to make sure
+// that we include it here, so that we can use it when necessary.
 dojo.require('shackleton.map');
 
 var __init__ = function () {
   
-  /**
-   * Check for ArcGIS Online Server Availibility
-   *
-   * Make sure we have loaded our classes/libraries
-   * properly and that we have access to the Esri
-   * mapping object and associated functions.
-   *
-   */
+  // Setup our initialization variables
+  var requestHandle,
+      _UIElements = "#progress";
+
+  
+  //
+  // Make sure we have loaded our classes/libraries
+  // properly and that we have access to the Esri
+  // mapping object and associated functions. 
+  //
   if (typeof esri == 'undefined') {
       console.error('[Map] Could not connect to server');
       return false;
   };
   
 try {
-  /**
-   * Check to see what parameters exist in the URL
-   *
-   * Make sure that we are capable of loading an application
-   * from ArcGIS Online. In order to do so we need either an
-   * appid or Application Idenitification Key, or we need
-   * a straight Webmap Identification Key.
-   *
-   */
+
+   // 
+   // Make sure that we are capable of loading an application
+   // from ArcGIS Online. In order to do so we need either an
+   // appid or Application Idenitification Key, or we need
+   // a straight Webmap Identification Key.
+   //
   defaults.query = esri.urlToObject(document.location.href).query;
 
-
-  /**
-   * Check to see how we should load our map
-   *
-   * If a WebMap ID is available through the URL object, then
-   * we should just go ahead and load the map.
-   *
-   */
+   // 
+   // If a WebMap ID is available through the URL object, then
+   // we should just go ahead and load the map.
+   //
   if ( defaults.query.webmap ) {
     defaults.webmap = defaults.query.webmap;
     var map = new shackleton.map(defaults);
   };
   
-  /**
-   * Check to see how we should load our map
-   *
-   * If an Application ID is available through the URL object, then
-   * we should use the Application ID or appid and load the WebMap ID
-   * through the ArcGIS Online Content/Data API to find out the rest 
-   * of the information about our Web Map.
-   *
-   */
+   // 
+   // If an Application ID is available through the URL object, then
+   // we should use the Application ID or appid and load the WebMap ID
+   // through the ArcGIS Online Content/Data API to find out the rest 
+   // of the information about our Web Map.
+   //
   if ( defaults.query.appid ) {
     requestHandle = esri.request({
         url: esri.arcgis.utils.arcgisUrl + "/" + defaults.query.appid + "/data",
@@ -75,12 +59,20 @@ try {
           for ( var key in response.values ) {
             defaults[key] = response.values[key];          
           }
+          //
+          // Here we actually instantiate our map and pass any defaults we have
+          // collected either from the application or the URL object.
+          //
           var map = new shackleton.map(defaults);
         },
+        //
+        // If we don't have anything (e.g., Web Map ID, Application ID) then there
+        // is no point in continuing, we should just stop everything, remove the
+        // "Loading map" progress bar and let the user know something is wrong.
+        // We'll display a message to them, giving them access to alternative maps
+        // or a way out of this broken page.
+        //
         error: function ( error ) {
-          var _UIElements = "#progress";
-    
-          console.error(error.message);
           jQuery(_UIElements).toggle();
         }
     });
@@ -89,17 +81,10 @@ try {
       
 } catch (e) {
 
-  /**
-   * Check to see if we have any query parameters in our URL
-   *
-   * If we don't have anything (e.g., Web Map ID, Application ID)
-   * then there is no point in continuing, we should just stop
-   * everything and let the user know something is wrong.
-   *
-   */
-  var _UIElements = "#progress";
-    
-  console.log('stop the presses', e);
+  //
+  // Again, same as the last comment, give the user something helpful to work with if
+  // there is nothing to display.
+  //
   jQuery(_UIElements).toggle();
 
 }
@@ -107,4 +92,30 @@ try {
         
 };
 
+//
+// Let's go ahead and start the application when Dojo decides that the page
+// is ready.
+//
 dojo.ready(__init__);
+
+//     Shackleton 0.1.01
+//
+//     Shackleton is a framework for building web map applications
+//     that are compatible with ArcGIS Online.
+//
+//     It is named in honor of the famous and widely respected
+//     British Antarctic explorer Sir Ernest Henry Shackleton.
+//
+//     Viable Industries, ArcGIS Compatible Web Application,
+//     where applicable. This application was built by the 
+//     folks at Viable Industries for use by it's clients only. 
+//     Any code herein is property of Viable Industries.
+//     unless where otherwise noted property of Esri/ArcGIS
+//
+//     To find out how you can use this software or have your
+//     own version of this software, custom built to fit your
+//     organizations needs, contact us by visiting
+//     www.viableindustries.com or emailing us at joshua@viable.io
+//
+//     Copyright (c) 2013 Viable Industries, All rights reserved.
+//     Unless otherwise noted.
--- js/shackleton/basemaps.js
+++ js/shackleton/basemaps.js
@@ -1,33 +1,52 @@
-/**
- * Developed Simple ArcGIS Web Application (c) 2013
- *
- *
- * This application was built by the folks at Viable for
- * use by it's clients. Any code herein is property of Developed
- * Simple unless where otherwise noted property of ESRI.
- *
- * To find out how you can use this software or have your
- * own version of this software built to fit your custom
- * needs, contact us by visiting www.developedsimple.com.
- *
- */
- 
-define(['dojo/_base/declare', 'esri/dijit/BasemapGallery' ], function(declare, esriDijitBasemap ){
+//
+// Visit the <a target="_blank" href="http://help.arcgis.com/en/webapi/javascript/arcgis/jsapi/#basemapgallery">ArcGIS Online API Documentation to learn more about
+// the Esri Basemaps Class and read further documentation</a>
+// 
 
-  var basemaps = declare('shackleton.basemaps', null, {
+// Make sure that we have ``declare`` and the ``BasemapGallery``
+define('SKBasemaps', ['dojo/_base/declare', 'esri/dijit/BasemapGallery' ], function(declare, esriDijitBasemap ) {
+
+  var SKBasemaps = declare('shackleton.basemaps', null, {
 
     constructor: function () {
 
+      //
+      // Instantiate our Basemap Gallery and make sure that
+      // we are using the basic set of ArcGIS Basemaps.
       var basemapsGallery = new esri.dijit.BasemapGallery({
         showArcGISBasemaps: true,
         map: map
+        // Add the Basemaps to the Basemaps modal
       }, 'basemaps-gallery');
 
+
+      // Startup the Basemap Gallery as soon as possible
       basemapsGallery.startup();
     }
 
   });
 
-  return basemaps;
+  return SKBasemaps;
 
 });
+
+//
+//     Shackleton is a framework for building web map applications
+//     that are compatible with ArcGIS Online.
+//
+//     It is named in honor of the famous and widely respected
+//     British Antarctic explorer Sir Ernest Henry Shackleton.
+//
+//     Viable Industries, ArcGIS Compatible Web Application,
+//     where applicable. This application was built by the 
+//     folks at Viable Industries for use by it's clients only. 
+//     Any code herein is property of Viable Industries.
+//     unless where otherwise noted property of Esri/ArcGIS
+//
+//     To find out how you can use this software or have your
+//     own version of this software, custom built to fit your
+//     organizations needs, contact us by visiting
+//     www.viableindustries.com or emailing us at joshua@viable.io
+//
+//     Copyright (c) 2013 Viable Industries, All rights reserved.
+//     Unless otherwise noted. 
