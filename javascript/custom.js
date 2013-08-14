11c1c3cd520c7c8f11d7a9df16ba6835c23c29e1
root <root@fractracker.org>
2013-08-14 12:51:07 -0400

    Update to the newest version of the basic viewer;

---
+++ javascript/custom.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *
+ *
+ */
+
+console.log('Custom loaded…');
+console.log(configOptions);
+
+details = document.getElementById('detailPanel');
+document.getElementById('detailPanel').innerHTML = '<h2>' + configOptions.title + '</h2>' + details;
